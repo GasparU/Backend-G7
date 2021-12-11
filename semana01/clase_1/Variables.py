3e2c520d799d83d1e6c12e3c3f9166e9d4efc63e
Jose Gaspar <sainc2016@gmail.com>
2021-12-10 21:48:38 -0500

    ejercicio

--- semana01/clase_1/Variables.py
+++ semana01/clase_1/Variables.py
@@ -1,7 +1,31 @@
-nombre="Jose"
-numero=0
-numero2= 10.5
-html='''<html>'''
-html2="""<html>"""
+personas = [{
+    'nombre': 'Eduardo',
+    'nacionalidad': 'peruano',
+    'hobbies':[
+        {
+            'nombre': 'Volar drones',
+            'experiencia': '2 años'
+        },{
+            'nombre':'Programar',
+            'experiencia': '1 mes'
+        }
+    ]
+},{
+    'nombre': 'Juliana',
+    'nacionalidad': 'colombiana',
+    'hobbies':[
+        {
+            'nombre': 'Montar bici',
+            'experiencia': '4 años'
+        },{
+            'nombre':'Bases de datos',
+            'experiencia': '8 mes'
+        }
+    ]
+}]
 
-print(type(numero2))
+print(personas[0]['nacionalidad'])
+print(personas[0]['hobbies'][1]['experiencia'])
+
+print(personas[1]['hobbies'][0]['nombre'])
+print(personas[1]['hobbies'][1]['nombre'])
