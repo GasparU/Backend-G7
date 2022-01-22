98969eecb5830c4b9cdf14489a5db36e96ffea98
Jose Gaspar <sainc2016@gmail.com>
2022-01-21 22:04:02 -0500

    tercera parte

---
+++ Sesion02/index.js
@@ -0,0 +1,12 @@
+import express from express;
+import prisma from "@prisma/client" ;
+const {PrismaClient} = prisma;
+
+const app = express()
+//nullish coalesching operator
+//valida el contenido de la izquierda y si es nulo o undefined entonces procedera tomar el valor de la derecha
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
+});
