385e3285d0c6b3241bec7be430443b9319f880e9
Jose Gaspar <sainc2016@gmail.com>
2022-01-26 18:33:48 -0500

    final semana 5

--- Sesion02/src/routes/categoria.routes.js
+++ Sesion02/src/routes/categoria.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import {
+  actualizarCategoria,
+  buscarCategoria,
   crearCategoria,
   listaCategoria,
 } from "../controllers/categoria.controler.js";
@@ -7,3 +9,7 @@ import {
 export const categoriaRouter = Router();
 
 categoriaRouter.route("/categoria").post(crearCategoria).get(listaCategoria);
+
+categoriaRouter.get("/buscarCategoria", buscarCategoria);
+
+categoriaRouter.route("/categoria/:id").put(actualizarCategoria);
