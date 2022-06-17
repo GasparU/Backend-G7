50eea114a5e71c93a1f7559f260d360c61d27a9d
Jose Gaspar <sainc2016@gmail.com>
2022-06-16 21:03:48 -0500

    primer dia

---
+++ dia1/variables.py
@@ -0,0 +1,11 @@
+# Esto es un comentario
+nombre = 'Eduardo'
+edad = 40
+# Para poder poner en diferentes lineas, no se usa el backstick sino triple comilla
+saludo = """Buenas amigos
+como estan
+Así podemos poner diversas lineas"""
+# Si no queremos darle aun un valor, se le crea con none. En JS se puede poner felicidad; o felicidad =
+# En python se hace mediante None.
+facilidad = None
+mascota1, mascota2, gatito1 = "morocha", "lulu", "venecia", "peluchin"
